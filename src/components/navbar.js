66078e204f512e19edb731c954de57dc3fece02a
Edward Lee <7edwardlee@gmail.com>
2023-11-24 00:58:34 -0800

    added hamburger navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
   return (
-    <ul className="navbar">
-      <li className="nav-item"><Link to="/login">Login</Link></li>
-      <li className="nav-item"><Link to="/favorites">Favorites</Link></li>
-      <li className="nav-item"><Link to="/upload">Upload</Link></li>
-      <li className="nav-item"><Link to="/articles">Articles</Link></li>
-      <li className="nav-item"><Link to="/">Home</Link></li>
-    </ul>
+    <div>
+      <div className="hamburger-icon" onClick={toggleMenu}>
+        &#9776;
+      </div>
+
+      <ul className={`navbar ${menuVisible ? 'active' : ''}`}>
+        <li className="nav-item"><Link to="/login">Login</Link></li>
+        <li className="nav-item"><Link to="/favorites">Favorites</Link></li>
+        <li className="nav-item"><Link to="/upload">Upload</Link></li>
+        <li className="nav-item"><Link to="/articles">Articles</Link></li>
+        <li className="nav-item"><Link to="/">Home</Link></li>
+      </ul>
+    </div>
   );
 };
 
--- src/favorites.js
+++ src/favorites.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import Card from './components/card.js';
+import React from 'react'
+import Card from './components/card';
+import aladdin from './img/aladdins.jpeg';
 
 const Favorites = () => {
-    return (
-        <div>
-            <h1>Favorites</h1>
-            <div className="top-container">
-                <div className="top-row">
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                </div>
-                <div className="top-row">
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                </div>
-            </div>
-        </div>
-    )
-}
+
+  const cardData = Array.from({ length: 6 }, (_, index) => ({
+    id: index + 1,
+    name: 'Aladdins',
+    price: '$',
+    distance: '0.1',
+    image: aladdin
+  }))
+
+  return (
+    <div className="top-container">
+      <div className="article-display">
+        {Array.from({ length: 6 }, (_, rowIndex) => (
+          <div key={rowIndex} className="card-row">
+            {cardData.slice(rowIndex * 3, rowIndex * 3 + 3).map((card) => (
+              <Card
+                key={card.id}
+                name={card.name}
+                price={card.price}
+                distance={card.distance}
+                image={card.image}
+                alt={card.name}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+
+  );
+};
 
 export default Favorites;
